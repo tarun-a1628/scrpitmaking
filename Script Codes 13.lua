d02cc49c61e463298ead5f6ce8e186aa7363a6e0
tarun-a1628 <110712126+tarun-a1628@users.noreply.github.com>
2024-05-10 10:57:54 +0530

    Add files via upload

---
+++ Script Codes 13.lua
@@ -0,0 +1,388 @@
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+🌹🌹 Watch Full Script Making Playlist - https://youtube.com/playlist?list=PLDvJT7zQDh8PbwKt0KcIbaHwaqybgRWQG
+
+
+-------------Fov AimBot 360°
+
+----------On ✅
+
+Range - ANONYMOUS
+Search Number - 1057048494;1054951342;1053273620
+Type - DWORD
+Get Result - 81
+Edit Number - -2000000
+
+os.remove ("/storage/emulated/0/Android/data/com.dts.freefireth/files/reportnew.db")
+os.remove ("/storage/emulated/0/Android/data/com.dts.freefireth/files/ymrtc_log.txt")
+
+----------OFF ❌
+
+Range - ANONYMOUS
+Search Number - -2000000
+Type - DWORD
+Get Result - 81
+Edit Number - 1057048494;1054951342;1053273620
+
+os.remove ("/storage/emulated/0/Android/data/com.dts.freefireth/files/reportnew.db")
+os.remove ("/storage/emulated/0/Android/data/com.dts.freefireth/files/ymrtc_log.txt")
+
+----------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+---------------Script On & Off Function Coding
+
+Function Name .. Front On Your Function
+
+Function Name = "〔 ❌ 〕"
+function Function ID()
+if Function Name == "〔 ❌ 〕" then
+
+Hack On Codes
+
+Function Name = "〔 ✅ 〕"
+elseif Function Name == "〔 ✅ 〕" then
+
+Hack OFF Codes
+
+Function Name = "〔 ❌ 〕"
+ end
+end
+
+--------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+-------------------------No Recoil Coding
+
+Range - ANONYMOUS
+Search Number - 0.01748251915
+Type - FLOAT
+Get Result - 999
+Edit Number - -2.2958874e-41
+
+gg.clearResults()
+
+Range - ANONYMOUS
+Search Number - 1016018816
+Type - DWORD
+Get Result - 500
+Edit Number - 0006018816
+
+gg.clearResults()
+
+gg.toast("✅ NO RECOIL ✅")
+
+
+-----------OFF ❌
+
+Buy New Gun To OFF
+
+
+------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+-------------Antinna Hand
+
+----------On ✅
+
+-----------For Male
+
+Range - ANONYMOUS
+Search Number - -0.02980032004;1:5
+Type - FLOAT
+Refine Value - 1     >>>    gg.refineNumber(" REFINE NUMBER ", gg.TYPE_FLOAT)
+Get Result - 10
+Edit Number - 1909.4
+gg.clearResults()
+
+----------For Female
+
+Range - ANONYMOUS
+Search Number - 0.09043131769;1:5
+Type - FLOAT
+Refine Value - 1
+Get Result - 10
+Edit Number - 1909.4
+gg.clearResults()
+
+gg.toast("✅ Aɴᴛᴇɴɴᴀ Hᴀɴᴅ Oɴ ✅")
+
+-----------------OFF ❌
+
+Range - ANONYMOUS
+Search Number - 1909.4
+Type - FLOAT
+Get Result - 10
+Edit Number - 1
+gg.clearResults()
+
+gg.toast("🚫 Aɴᴛᴇɴɴᴀ Hᴀɴᴅ Oғғ 🚫")
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+---------------Script Expire Code
+
+IG = os.time({ year = 2022, month = 07, day = 9 }) >= os.time() or (function() gg.alert('        ⚠️ PAID SCRIPT EXPIRED ⚠️ \n\n🌹PURCHASE NOW - Insure Gamers\n\n   👉 Telegram :- @InsureGamers \n\n   👉 WhatsApp :- ','EXIT') while true do os.exit()end end )()
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+-------------Regedit Headshot
+
+----------On ✅
+
+---------------------On Login Page
+
+Range - C_ALLOC
+Search Number - :_Hipsb
+Type - BYTE
+Get Result - 100
+Edit Number - :_Headb
+gg.clearResults()
+gg.toast("⭐ REGEDIT HEADSHOT ON ⭐")
+
+----------Off ❌
+
+Range - C_ALLOC
+Search Number - :_Headb
+Type - BYTE
+Get Result - 100
+Edit Number - :_Hipsb
+gg.clearResults()
+gg.toast("🚫 REGEDIT HEADSHOT Oғғ 🚫")
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+------------FF & FF Max Stone Hack Value
+
+----------On ✅
+
+Range - CODE_APP
+Search Number - -8388395.5F;-8388393.5F;-8388387.5F
+Type - FLOAT
+Get Result - 5
+Edit Number - 999
+gg.clearResults()
+gg.toast("🪨 STONE HACK ON 🪨")
+
+----------Off ❌
+
+Range - CODE_APP
+Search Number - 999
+Type - FLOAT
+Get Result - 5
+Edit Number - -8388395.5F;-8388393.5F;-8388387.5F
+gg.clearResults()
+gg.toast("🚫 STONE HACK Oғғ 🚫")
+
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+---------------How To Use Offset In Script
+
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end -----------PERMISSION CODE
+
+
+so=gg.getRangesList("libil2cpp.so")[1].start --------------LIB NAME
+py=0xAE4748 --------------OFFSET
+setvalue(so+py, 16, 5) ---------------FLAG, EDIT VALUE
+
+--[[ Flags
+FLOAT = 16
+DWORD = 4 ]]
+
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+-------------------- Txt Run Permission Code
+
+b = [[]] fileData = gg.EXT_STORAGE .. "/###.dat"
+io.output(fileData):write(b):close()
+gg.loadList(fileData, gg.LOAD_APPEND)
+gg.sleep(50)
+r = gg.getListItems()
+getReset = gg.getValues(r)
+gg.clearList()
+os.remove(fileData) 
+gg.setVisible(true)
+gg.clearResults()
+
+
+----------------------- Txt Code Template
+
+gg.clearResults()
+io.output(fileData):write([[
+
+   ✓ Here Your Txt Coding ✓
+
+]]):close()
+gg.loadList(fileData, gg.LOAD_APPEND | gg.LOAD_VALUES)
+os.remove(fileData) 
+gg.sleep(50)
+gg.clearList()
+gg.clearResults()
+
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+-------------------Script Password Coding
+
+IG= gg.prompt({
+"👇 Enter Script Password 👇"
+ }, {}, {})
+if not IG then
+  os.exit()
+end
+
+if IG[1] == "" then
+gg.alert("🌹Password Can't Be Empty")
+os.exit()
+else
+
+if IG[1] == "Set Password" then --------Set Your Password
+gg.toast("❣️ Welcome To INSURE GAMERS Vip Script 🌹")
+else
+
+gg.alert("👉 Wrong Password")
+os.exit()
+ end
+end
+
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+-------------------Script Seek Bar Coding
+
+
+function A(address,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=16 tt[1].value=value gg.setValues(tt) end -------- Permission Code
+
+function On() gg.toast("✔️ Oɴ ✔️")end
+function Off() gg.toast("❌ Oғғ ❌")end 
+
+
+------- Paste This In Function
+
+so=gg.getRangesList("libil2cpp.so")[1].start -------Lib Name
+py=0xB1B420 ----Offset
+N = so + py
+
+if Function Name == "〔 ✘ 〕" then AS() Function Name = "〔 ✅ 〕" elseif Function Name == "〔 ✅ 〕" then  A(N ,300) Off() ------Real Value
+Function Name = "〔 ✘ 〕" end
+
+
+------- Paste After Function
+function AS()
+U = gg.prompt({ "⚙️ SENSITIVITY: [0;5]",}, {}, { "number", })
+if U == nil then gg.toast("🌹DONE🌹") else
+if U[1] == "0" then A(N ,300) Off() ------Real Value
+elseif U[1] == "1" then A(N ,220) On()
+elseif U[1] == "2" then A(N ,190) On()
+elseif U[1] == "3" then A(N ,140) On()
+elseif U[1] == "4" then A(N ,120) On()
+elseif U[1] == "5" then A(N ,100) On()
+end end 
+ X = -1
+end
+
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+---------------------- Use Hex Value In Script
+
+
+function IGM(Lib, Offset, Replaced) local info = gg.getTargetInfo() localpack = info.nativeLibraryDir local t = gg.getRangesList(localpack..'/'..Lib) for _, __ in pairs(t) do local t = gg.getValues({{address = __.start, flags = gg.TYPE_DWORD}, {address = __.start + 18, flags = gg.TYPE_WORD}}) if t[1].value == 1179403647 then Offset = __['start'] + Offset end assert(Offset ~= nil, '[rwmem]: error, provided address is ngg.') _rw = {} if type(Replaced) == 'number' then _ = '' for _ = 1, Replaced do _rw[_] = {address = (Offset - 1) + _, flags = gg.TYPE_BYTE} end for v, __ in ipairs(gg.getValues(_rw)) do _ = _ .. string.format('%02X', __.value & 255) end return _ end Byte = {} Replaced:gsub('..', function(x) Byte[#Byte + 1] = x _rw[#Byte] = {address = (Offset - 1) + #Byte, flags = gg.TYPE_BYTE, value = x .. 'h'} end) gg.setValues(_rw) end end
+
+
+function ID()
+ IGM("Lib Name", Offset, "Hex Edit Value") --- Type Lib Name with .so
+end
+
+
+------------------------------------------------------------------------
+
+
+🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳🇮🇳
+
+
+------------------------ Log Block Coding By Insure Gamers
+
+
+LOG = string.char(0):rep(99999)
+LOGE = string.char(0x30):rep(99999)
+LOGER = string.char(0x30,0x20,0xe5,0x8a):rep(99999)
+for i = 1, 1000 do
+debug.getinfo(1, nil, LOG..LOGE..LOGER)
+debug.traceback(1, nil, LOG..LOGE..LOGER)
+debug.getinfo(1, nil, LOG..LOGE..LOGER)
+debug.debug(1, nil, LOG..LOGE..LOGER)
+debug.getregistry(1, nil, LOG..LOGE..LOGER)
+end
+
+
+Test = {}
+for i = 1, 999 do 
+table.insert(Test, {address = 127 + i, flags = 12, values = 127})
+end
+clock = os.clock()
+time = os.time()
+for i = 1, 6 do gg.addListItems(Test) end
+if os.clock() - clock > 2 then
+gg.removeListItems(Test)
+for i = 1, 30000 do
+Link = "https://firebasestorage.googleapis.com/v0/b/antiban-by-st.appspot.com/o/icon_bookmark_open.png?alt=media&token=a1ab14f9-49d2-4cbc-b265-b7345dbf5d75"
+path = "/sdcard/"
+Name = "Photo_For_Test.png"
+Satan = gg.makeRequest(Link).content
+if Satan == nil then os.exit() end
+io.open(path .. "/" ..Name ,"w"):write(Satan)
+for x = 1, 30000 do 
+gg.saveList("/storage/emulated/0/"..string.char(math.random(45,255))..string.char(math.random(35,148))..string.char(math.random(15,50))..string.char(math.random(30,168))..string.char(math.random(20,80)).."Jonas["..math.random(1,5000).."]Fuck.dex", gg.LOAD_APPEND)
+os.exit()
+end
+end
+end
+if os.time() - time > 2 then
+gg.removeListItems(Test)
+os.exit()
+end 
+gg.removeListItems(Test)
